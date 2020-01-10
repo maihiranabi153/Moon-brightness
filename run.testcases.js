4d79f660d16e3099fbb8d4ee75153682f0030ce4
Maihiranabi Hanagikatt <59684544+maihiranabi153@users.noreply.github.com>
2020-01-10 19:05:14 +0530

    Delete run.testcases.js

--- run.testcases.js
+++
@@ -1,7 +0,0 @@
-require('./createparkslot.test.js');
-require('./park.test.js');
-require('./leavepark.test.js');
-require('./parkingstatus.test.js');
-require('./registration_numbers_for_cars_with_colour.test.js');
-require('./slot_numbers_for_cars_with_colour.test.js')
-require('./slot_number_for_registration_number.test.js');
